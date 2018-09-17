3ce745a206a670b14a41c0dd4fcd4289962b871f
senorsam44 <samsikder47@gmail.com>
2018-09-18 01:57:47 +0600

    Initial commit

---
+++ app/src/main/java/com/example/senor/bkv3/uc3_signup.java
@@ -0,0 +1,65 @@
+package com.example.senor.bkv3;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link uc3_signup#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class uc3_signup extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    public uc3_signup() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment uc3_signup.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static uc3_signup newInstance(String param1, String param2) {
+        uc3_signup fragment = new uc3_signup();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View contentview = inflater.inflate(R.layout.fragment_uc3_signup, container, false);
+        return contentview;
+    }
+
+}
